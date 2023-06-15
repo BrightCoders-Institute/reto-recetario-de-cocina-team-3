ec6e7c68c38eb3bf81c26a7f20a4084ea11d5aea
Ramon Ibarra <ramon.ibrr@gmail.com>
2023-06-14 18:03:16 -0600

    Add more products and Scrollview implementation
    Co-authored-by: madera-design<josemadera289@gmail.com>
    -authored-by: ShadowEnchanter<enlace.gustavolopez@gmail.com>

--- RecetarioCocina/src/components/Menu.tsx
+++ RecetarioCocina/src/components/Menu.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { View } from 'react-native';
+import React from 'react';
+import { ScrollView } from 'react-native';
 import { MenuData } from '../interfaces/menuData';
 import { Card } from './Card';
 
 export const Menu = ({menuData} : MenuData) => {
 	return (
-		<View style={{flexDirection:"row"}}>
+		<ScrollView horizontal={true}>
 			{
 				menuData.map((item) => (
-					<Card urlImg={item.img} 
-								description={item.Description} 
+					<Card urlImg={item.img}
+								description={item.Description}
 								key={item.index}
 								dimensions={item.imgDimensions}
 							/>
 				))
 			}
-		</View>
-	)
-}
+		</ScrollView>
+	);
+};
