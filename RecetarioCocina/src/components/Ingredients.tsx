9a12a8cab9ca266c713d5b53b5e768fac3f51d80
jose madera <josemadera289@gmail.com>
2023-06-16 18:36:17 -0600

    update info food

--- RecetarioCocina/src/components/Ingredients.tsx
+++ RecetarioCocina/src/components/Ingredients.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export const Ingredients = ({item}) => {
+interface propsIngredient {
+	dataItem: {}
+}
 
-  const { id, portions, ingredient1, ingredient2, ingredient3, ingredient4, amount1, amount2, amount3, amount4 } = item
-
+export const Ingredients = ({dataItem}: propsIngredient) => {
+    console.log(dataItem);
   return (
     <View>
-        <Text style={{fontSize: 16, fontWeight: 'bold', }}>{portions}</Text>
+        {/* <Text style={{fontSize: 16, fontWeight: 'bold', }}>{portion}</Text>
         <View style={style.container}>
-            <Text style={style.ingredient}>{ingredient1}</Text>
-            <Text style={style.amount}>{amount1}</Text>
-        </View>
-        <View style={style.container}>
-            <Text style={style.ingredient}>{ingredient2}</Text>
-            <Text style={style.amount}>{amount2}</Text>
-        </View>
-        <View style={style.container}>
-            <Text style={style.ingredient}>{ingredient3}</Text>
-            <Text style={style.amount}>{amount3}</Text>
-        </View>
-        <View style={style.container}>
-            <Text style={style.ingredient}>{ingredient4}</Text>
-            <Text style={style.amount}>{amount4}</Text>
-        </View>
+            {
+                ingredient.map((item_ingredient) => <Text style={style.ingredient}>{item_ingredient}</Text>)
+            },
+            {
+                amount.map((item_amount) => (
+                    <Text style={style.amount}>{item_amount}</Text>
+                ))
+            }
+        </View> */}
     </View>
   )
 }
--- RecetarioCocina/src/screens/Details.tsx
+++ RecetarioCocina/src/screens/Details.tsx
@@ -4,117 +4,27 @@ import {ImgFOOD} from '../components/ImgFood';
 import { FlatList } from 'react-native-gesture-handler';
 import { Ingredients } from '../components/Ingredients';
 
-const INGREDIENTS_LIST = [
-	{
-		id: 1,
-		portions: '4 people',
-		ingredient1: 'Flour',
-		ingredient2: 'Tomatoe Sauce',
-		ingredient3: 'Cheese',
-		ingredient4: 'Peperonni',
-		amount1: '1000 gr.',
-		amount2: '100 gr.',
-		amount3: '120 gr.',
-		amount4: '35 gr.',
-	},
-	{
-		id: 2,
-		portions: '4 pieces',
-		ingredient1: 'Flour',
-		ingredient2: 'Wather',
-		ingredient3: 'Yeast',
-		ingredient4: 'Chips',
-		amount1: '800 gr.',
-		amount2: '250 ml.',
-		amount3: '150 gr.',
-		amount4: '20 gr.',
-	},
-	{
-		id: 3,
-		portions: '4 pieces',
-		ingredient1: 'Flour',
-		ingredient2: 'Wather',
-		ingredient3: 'Cocoa',
-		ingredient4: 'Chips',
-		amount1: '800 gr.',
-		amount2: '250 ml.',
-		amount3: '200 gr.',
-		amount4: '20 gr.',
-	},
-	{
-		id: 4,
-		portions: '1 Burger',
-		ingredient1: 'Bread',
-		ingredient2: 'Beef',
-		ingredient3: 'Lettuce',
-		ingredient4: 'Yellow Cheese',
-		amount1: '1 pair',
-		amount2: '1 pcs.',
-		amount3: '1 pcs.',
-		amount4: '1 pcs.',
-	},
-	{
-		id: 5,
-		portions: '3 tacos',
-		ingredient1: 'Tortilla',
-		ingredient2: 'Barbacoa Meat',
-		ingredient3: 'Onion',
-		ingredient4: 'Cilantro',
-		amount1: '3 pcs.',
-		amount2: 'to taste :)',
-		amount3: 'to taste :)',
-		amount4: 'to taste :)',
-	},
-	{
-		id: 6,
-		portions: '4 people',
-		ingredient1: 'Ground meat',
-		ingredient2: 'Sauce',
-		ingredient3: 'Seasonings',
-		ingredient4: 'Cilantro',
-		amount1: '480 gr.',
-		amount2: '800 ml.',
-		amount3: 'to taste :)',
-		amount4: 'to taste :)',
-	},
-	{
-		id: 7,
-		portions: '4 people',
-		ingredient1: 'Macaroni',
-		ingredient2: 'Tomatoes Sauces',
-		ingredient3: 'Seasonings',
-		ingredient4: 'Cream',
-		amount1: '800 gr.',
-		amount2: '500 gr.',
-		amount3: 'to taste :)',
-		amount4: '200 gr.',
-	},
-	{
-		id: 8,
-		portions: '4 portions',
-		ingredient1: 'Flour',
-		ingredient2: 'Milk',
-		ingredient3: 'Bitumen',
-		ingredient4: 'Chips',
-		amount1: '1000 gr.',
-		amount2: '500 ml.',
-		amount3: '150 gr. ',
-		amount4: '50 gr.',
-	},
-]
 
-export const Details = () => {
+export const Details = ({route}) => {
+	const data_item =  route.params.data;
+	console.log('hola 2');
 	return(
-		<View style={{backgroundColor: '#242424', }}>
-			{/* <ImgFOOD
-				category={data.category}
-				title={data.Description}
-				img={data.img}
-			/> */}
+		<View style={{backgroundColor: '#242424'}}>
+			<ImgFOOD
+				category={data_item.category}
+				title={data_item.Description}
+				img={data_item.img}
+			/>
+			{/* <Text>{data_item.portion}</Text>
+			<Text>{data_item.ingredient}</Text> */}
 			<FlatList
-			data={INGREDIENTS_LIST}
-			keyExtractor={item => item.id}
-			renderItem={({item}) => <Ingredients item={item} />}
+			data={data_item}
+			keyExtractor={item => item.index}
+			renderItem={({item}) => 
+				<Ingredients 
+					dataItem = {item}
+				/>
+			}
 			ItemSeparatorComponent = {() => <View style = {{marginBottom: 20, }} /> }
 			ListHeaderComponent={
 				<View>
